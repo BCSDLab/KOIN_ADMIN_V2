af3ee07292fe9af0e6a5ac84989395a85c6c9bd7
WonBeen Choi <zad1264@naver.com>
2022-10-03 22:25:46 +0900

    feat: SideNav 목록 생성

--- src/components/common/SideNav/index.tsx
+++ src/components/common/SideNav/index.tsx
@@ -1,5 +1,10 @@
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import {
+  AppstoreOutlined, UserOutlined, CarOutlined, ShopOutlined,
+  CoffeeOutlined, HomeOutlined, UserSwitchOutlined,
+  UsergroupDeleteOutlined, FolderOpenOutlined, ControlOutlined, BarChartOutlined,
+} from '@ant-design/icons';
 import { Menu, MenuProps } from 'antd';
+import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 type MenuItem = Required<MenuProps>['items'][number];
@@ -21,22 +26,21 @@ function getItem(
 }
 
 const items: MenuProps['items'] = [
-  getItem('코인 서비스', 'sub1', <MailOutlined />, [
-    getItem('Item 1', 'g1', null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
-    getItem('Item 2', 'g2', null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
+  getItem('대시보드', '/', <BarChartOutlined />),
+
+  getItem('코인 서비스', 'service', <AppstoreOutlined />, [
+    getItem('주변상점', 'service-store', <ShopOutlined />, [
+      getItem('상점 관리', '/store', <ControlOutlined />),
+      getItem('카테고리', '/category', <FolderOpenOutlined />),
+    ]),
+    getItem('학교식단', '/cafeteria', <CoffeeOutlined />),
+    getItem('버스 정보', '/bus', <CarOutlined />),
+    getItem('복덕방', '/room', <HomeOutlined />),
   ]),
 
-  getItem('회원 관리', 'sub2', <AppstoreOutlined />, [
-    getItem('학생 회원', '5'),
-    getItem('사장님', '6'),
-    getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
-  ]),
-
-  getItem('Navigation Three', 'sub4', <SettingOutlined />, [
-    getItem('Option 9', '9'),
-    getItem('Option 10', '10'),
-    getItem('Option 11', '11'),
-    getItem('Option 12', '12'),
+  getItem('회원 관리', 'user', <UserOutlined />, [
+    getItem('학생 회원', '/user', <UsergroupDeleteOutlined />),
+    getItem('사장님', '/manager', <UserSwitchOutlined />),
   ]),
 ];
 
@@ -47,15 +51,17 @@ const SideNavConatiner = styled.nav`
 `;
 
 function SideNav() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
   const onClick: MenuProps['onClick'] = (e) => {
-    console.log('click ', e);
+    navigate(e.key);
   };
   return (
     <SideNavConatiner>
       <Menu
         onClick={onClick}
-        defaultSelectedKeys={['1']}
-        defaultOpenKeys={['sub1']}
+        selectedKeys={[pathname]}
+        defaultOpenKeys={['service', 'user']}
         style={{ height: '100vh' }}
         mode="inline"
         items={items}

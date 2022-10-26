c9102959159c4a70ae4a061aeb404007b80b7e72
hana <rlagksk8428@gmail.com>
2022-10-26 22:52:11 +0900

    feat: room detail 페이지 추가

---
+++ src/pages/Services/Room/RoomDetail.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './RoomDetail.style';
+
+export default function RoomDetail() {
+  return (
+    <S.Container>
+      <S.Heading>RoomDetail</S.Heading>
+    </S.Container>
+  );
+}

260253ef77d3d83e6d8caaec9f4eaf9db5d299a0
hana <rlagksk8428@gmail.com>
2022-11-01 20:05:19 +0900

    feat: 복덕방 상세 페이지 타입 지정

--- src/model/room.model.ts
+++ src/model/room.model.ts
@@ -1,10 +1,35 @@
 export interface RoomDetail {
+  address: string;
+  charter_fee: string | number;
+  deposit: string | number;
+  description: string;
+  floor: string | number;
   id: number;
+  image_urls: string;
+  latitude: number;
+  longitude: number;
+  management_fee: string | number;
+  monthly_fee: string;
   name: string;
+  opt_air_conditioner: boolean;
+  opt_bed: boolean;
+  opt_bidet: boolean;
+  opt_closet: boolean;
+  opt_desk: boolean;
+  opt_electronic_door_locks: boolean;
+  opt_elevator: boolean;
+  opt_gas_range: boolean;
+  opt_induction: boolean;
+  opt_microwave: boolean;
+  opt_refrigerator: boolean;
+  opt_shoe_closet: boolean;
+  opt_tv: boolean;
+  opt_veranda: boolean;
+  opt_washer: boolean;
+  opt_water_purifier: boolean;
+  phone: string | number;
   room_type: string;
-  monthly_fee: number | string;
-  charter_fee: number | null;
-  // detail data 추가
+  size: number;
 }
 
 export interface RoomTableHead {
@@ -17,5 +42,5 @@ export interface RoomTableHead {
 
 export interface RoomResponse {
   totalPage: number;
-  lands: RoomDetail[];
+  lands: RoomTableHead[];
 }

447803bc6d1b57002fa4ab15aefe9955aac828af
hana <rlagksk8428@gmail.com>
2022-11-03 19:13:30 +0900

    feat: 복덕방 상세 페이지 스타일 적용

--- src/pages/Services/Room/RoomDetail.style.tsx
+++ src/pages/Services/Room/RoomDetail.style.tsx
@@ -1,5 +1,52 @@
+import { Form } from 'antd';
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  font-family: 'Avenir', 'Helvetica', 'Arial', 'sans-serif';
+`;
 
-export const Heading = styled.div``;
+export const Heading = styled.div`
+  color: rgb(44 62 80);
+  font-size: 28px;
+`;
+
+export const SubHeading = styled.div`
+  font-size: 13px;
+  padding: 12px 0;
+`;
+
+export const FormWrap = styled.div`
+  padding: 50px 60px;
+  .ant-form-item-label > label {
+    font-size: 15px;
+    font-weight: 600;
+    color: rgb(44 62 80);
+  }
+
+  .ant-divider-with-text {
+    font-size: 15px;
+    font-weight: 600;
+    color: rgb(44 62 80);
+  }
+
+  .ant-checkbox + span {
+    padding: 10px 39px 10px 10px;
+    font-size: 15px;
+    font-weight: 500;
+    color: rgb(44 62 80);
+  }
+
+  .ant-checkbox-wrapper + .ant-checkbox-wrapper {
+    margin-left: 0;
+  }
+`;
+
+export const FormItem = styled(Form.Item)`
+  margin-left: 25px;
+  margin-bottom: 50px;
+`;
+
+export const CheckboxWrap = styled.div`
+  padding: 15px 10px;
+  width: 45vw;
+`;

a759ef60180b6bce573d6f71918d23e7521df7fe
hana <rlagksk8428@gmail.com>
2022-11-30 10:16:18 +0900

    chore: toast 컴포넌트삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import Toast from 'components/common/Toast';
 import DefaultLayout from 'layout/defaultLayout';
 import Bus from 'pages/Services/Bus';
 import Cafeteria from 'pages/Services/Cafeteria';
@@ -34,27 +33,24 @@ function RequireAuth({ children }: { children: ReactNode }) {
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<RequireAuth><DefaultLayout /></RequireAuth>}>
-          <Route index element={<Dashboard />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/cafeteria" element={<Cafeteria />} />
-          <Route path="/bus" element={<Bus />} />
-          <Route path="/room" element={<RoomList />} />
-          <Route path="/room/:id" element={<RoomDetail />} />
-          <Route path="/user" element={<UserList />} />
-          <Route path="/user/:id" element={<UserDetail />} />
-          <Route path="/manager" element={<Manager />} />
-          <Route path="/manager-request" element={<ManagerRequest />} />
-          <Route path="/member" element={<Member />} />
-          <Route path="*" element={<h1>404</h1>} />
-        </Route>
-      </Routes>
-      <Toast />
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<RequireAuth><DefaultLayout /></RequireAuth>}>
+        <Route index element={<Dashboard />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/cafeteria" element={<Cafeteria />} />
+        <Route path="/bus" element={<Bus />} />
+        <Route path="/room" element={<RoomList />} />
+        <Route path="/room/:id" element={<RoomDetail />} />
+        <Route path="/user" element={<UserList />} />
+        <Route path="/user/:id" element={<UserDetail />} />
+        <Route path="/manager" element={<Manager />} />
+        <Route path="/manager-request" element={<ManagerRequest />} />
+        <Route path="/member" element={<Member />} />
+        <Route path="*" element={<h1>404</h1>} />
+      </Route>
+    </Routes>
   );
 }
 

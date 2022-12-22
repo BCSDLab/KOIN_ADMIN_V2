abf77d2741a1db6821acf9af40bfeb91e5a63710
hana <rlagksk8428@gmail.com>
2022-12-23 01:02:58 +0900

    fix: room detail initialvalue로 수정

--- src/pages/Services/Room/RoomDetail.tsx
+++ src/pages/Services/Room/RoomDetail.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import getDefaultValueArr from 'utils/ts/getDefaultValueArr';
+// import getDefaultValueArr from 'utils/ts/getDefaultValueArr';
 import { useGetRoomQuery } from 'store/api/room';
 import CustomForm from 'components/common/CustomForm';
 import { DeleteOutlined, UploadOutlined } from '@ant-design/icons';
@@ -13,31 +13,35 @@ export default function RoomDetail() {
   const { id } = useParams();
   const { data: roomRes } = useGetRoomQuery(Number(id));
   const { updateRoomDetail } = useRoomMutation(Number(id));
-  const defaultValueArr = getDefaultValueArr(roomRes);
+  // const defaultValueArr = getDefaultValueArr(roomRes);
   const { deleteRoomData } = useRoomMutation(Number(id));
   const [form] = CustomForm.useForm();
+  console.log(roomRes);
 
   return (
     <S.Container>
       <S.Heading>Room Detail</S.Heading>
       <S.SubHeading>Home / Room / RoomDetail</S.SubHeading>
       <S.FormWrap>
-        <CustomForm
-          onFinish={updateRoomDetail}
-          form={form}
-          fields={defaultValueArr}
-        >
-          <Divider orientation="left">기본 정보</Divider>
-          <DetailForm />
-          <S.ButtonWrap>
-            <CustomForm.Button icon={<UploadOutlined />} htmlType="submit">
-              완료
-            </CustomForm.Button>
-            <CustomForm.Button danger icon={<DeleteOutlined />} onClick={deleteRoomData}>
-              삭제
-            </CustomForm.Button>
-          </S.ButtonWrap>
-        </CustomForm>
+        {roomRes
+          && (
+          <CustomForm
+            onFinish={updateRoomDetail}
+            form={form}
+            initialValues={roomRes}
+          >
+            <Divider orientation="left">기본 정보</Divider>
+            <DetailForm />
+            <S.ButtonWrap>
+              <CustomForm.Button icon={<UploadOutlined />} htmlType="submit">
+                완료
+              </CustomForm.Button>
+              <CustomForm.Button danger icon={<DeleteOutlined />} onClick={deleteRoomData}>
+                삭제
+              </CustomForm.Button>
+            </S.ButtonWrap>
+          </CustomForm>
+          )}
       </S.FormWrap>
     </S.Container>
   );

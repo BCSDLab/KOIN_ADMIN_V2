6d13cd9edb82530294c369f53714efeb88f0731b
hana <rlagksk8428@gmail.com>
2022-12-29 19:42:50 +0900

    fix: room mutation 함수명 수정

--- src/pages/Services/Room/useRoomMutation.tsx
+++ src/pages/Services/Room/useRoomMutation.tsx
@@ -4,18 +4,18 @@ import { useNavigate } from 'react-router-dom';
 import { useDeleteRoomMutation, useUpdateRoomMutation } from 'store/api/room';
 
 export default function useRoomMutation(id: number) {
-  const [updateRoomRequest] = useUpdateRoomMutation();
-  const [deleteRoomRequest] = useDeleteRoomMutation();
+  const [updateRoomMutation] = useUpdateRoomMutation();
+  const [deleteRoomMutation] = useDeleteRoomMutation();
   const navigate = useNavigate();
 
   function deleteRoom() {
-    deleteRoomRequest(id);
+    deleteRoomMutation(id);
     message.error('삭제되었습니다.');
     navigate(-1);
   }
 
   function updateRoom(values: Partial<RoomResponse>) {
-    updateRoomRequest({ id, ...values });
+    updateRoomMutation({ id, ...values });
     message.success('정보 수정이 완료되었습니다.');
     navigate(-1);
   }

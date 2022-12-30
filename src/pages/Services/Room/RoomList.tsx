8052e815ac6f5d656f4686573faa0dfa74e4a7b4
hana <rlagksk8428@gmail.com>
2022-12-30 22:20:47 +0900

    fix: room 추가 modal button 위치 변경

--- src/pages/Services/Room/RoomList.tsx
+++ src/pages/Services/Room/RoomList.tsx
@@ -1,17 +1,24 @@
 import CustomTable from 'components/common/CustomTable';
 import { useState } from 'react';
 import { useGetRoomListQuery } from 'store/api/room';
+import CustomForm from 'components/common/CustomForm';
+import { PlusOutlined } from '@ant-design/icons';
+import useBooleanState from 'utils/hooks/useBoolean';
 import * as S from './RoomList.style';
 import RoomModal from './RoomModal';
 
 function RoomList() {
   const [page, setPage] = useState(1);
   const { data: roomRes } = useGetRoomListQuery(page);
+  const { setTrue: openModal } = useBooleanState();
 
   return (
     <S.Container>
       <S.Heading>Room</S.Heading>
-      <RoomModal />
+      <S.ModalWrap>
+        <CustomForm.Button icon={<PlusOutlined />} onClick={openModal}>생성</CustomForm.Button>
+        <RoomModal />
+      </S.ModalWrap>
       {roomRes && (
         <CustomTable
           data={roomRes.roomList}
--- src/pages/Services/Room/RoomModal.tsx
+++ src/pages/Services/Room/RoomModal.tsx
@@ -1,4 +1,4 @@
-import { PlusOutlined, UploadOutlined } from '@ant-design/icons';
+import { UploadOutlined } from '@ant-design/icons';
 import { Modal } from 'antd';
 import CustomForm from 'components/common/CustomForm';
 import { RoomResponse } from 'model/room.model';
@@ -8,7 +8,7 @@ import DetailForm from './DetailForm';
 import * as S from './RoomList.style';
 
 export default function RoomModal() {
-  const { isValue: isModalOpen, setTrue: openModal, setFalse: closeModal } = useBooleanState();
+  const { isValue: isModalOpen, setFalse: closeModal } = useBooleanState();
   const [addRoom] = useAddRoomMutation();
   const [form] = CustomForm.useForm();
 
@@ -18,30 +18,27 @@ export default function RoomModal() {
   };
 
   return (
-    <S.ModalWrap>
-      <CustomForm.Button icon={<PlusOutlined />} onClick={openModal}>생성</CustomForm.Button>
-      <Modal
-        title="등록하기"
-        open={isModalOpen}
-        onCancel={closeModal}
-        centered
-        width={900}
-        footer={null}
+    <Modal
+      title="등록하기"
+      open={isModalOpen}
+      onCancel={closeModal}
+      centered
+      width={900}
+      footer={null}
+    >
+      <CustomForm
+        onFinish={createRoom}
+        form={form}
       >
-        <CustomForm
-          onFinish={createRoom}
-          form={form}
-        >
-          <S.DetailFormWrap>
-            <DetailForm />
-            <S.SubmitButtonWrap>
-              <CustomForm.Button icon={<UploadOutlined />} htmlType="submit">
-                완료
-              </CustomForm.Button>
-            </S.SubmitButtonWrap>
-          </S.DetailFormWrap>
-        </CustomForm>
-      </Modal>
-    </S.ModalWrap>
+        <S.DetailFormWrap>
+          <DetailForm />
+          <S.SubmitButtonWrap>
+            <CustomForm.Button icon={<UploadOutlined />} htmlType="submit">
+              완료
+            </CustomForm.Button>
+          </S.SubmitButtonWrap>
+        </S.DetailFormWrap>
+      </CustomForm>
+    </Modal>
   );
 }

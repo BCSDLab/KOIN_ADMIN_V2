accf0df7e13ff221ea6bf1f42fb4b4c3e50d1573
hana <rlagksk8428@gmail.com>
2023-01-31 17:10:49 +0900

    feat: mutation에 deleteMember 추가

--- src/pages/UserManage/Member/useMemberMutation.tsx
+++ src/pages/UserManage/Member/useMemberMutation.tsx
@@ -1,15 +1,16 @@
 import { message } from 'antd';
 import { Member } from 'model/member.model';
 import { useNavigate } from 'react-router-dom';
-import { useUpdateMemberMutation } from 'store/api/member';
+import { useDeleteMemberMutation, useUpdateMemberMutation } from 'store/api/member';
 
-export default function useMemberMutation() {
-  const [updateMemberRequest] = useUpdateMemberMutation();
+export default function useMemberMutation(id: number) {
+  const [updateMemberMutation] = useUpdateMemberMutation();
+  const [deleteMemberMutation] = useDeleteMemberMutation();
   const navigate = useNavigate();
 
-  const updateMember = (formData: Member) => {
+  const updateMember = (formData: Partial<Member>) => {
     if (formData) {
-      updateMemberRequest(formData)
+      updateMemberMutation({ id, ...formData })
         .unwrap()
         .then(() => {
           message.success('정보 수정이 완료되었습니다.');
@@ -21,5 +22,16 @@ export default function useMemberMutation() {
     }
   };
 
-  return { updateMember };
+  function deleteMember() {
+    deleteMemberMutation(id)
+      .unwrap()
+      .then(() => {
+        message.success('삭제되었습니다.');
+        navigate(-1);
+      }).catch((({ data }) => {
+        message.error(data.error.message);
+      }));
+  }
+
+  return { updateMember, deleteMember };
 }

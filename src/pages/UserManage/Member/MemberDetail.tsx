df1106bad84b500c78b95fdbe3afc3a640ea8aeb
hana <rlagksk8428@gmail.com>
2023-02-04 17:27:04 +0900

    fix: track, position select로 변경

--- src/pages/UserManage/Member/MemberDetail.tsx
+++ src/pages/UserManage/Member/MemberDetail.tsx
@@ -4,6 +4,7 @@ import CustomForm from 'components/common/CustomForm';
 import { useParams } from 'react-router-dom';
 import DetailHeading from 'components/common/DetailHeading';
 import { useGetMemberQuery } from 'store/api/member';
+import { SELECT_OPTIONS } from 'constant/member';
 import * as S from './MemberDetail.style';
 import useMemberMutation from './useMemberMutation';
 
@@ -32,8 +33,8 @@ function MemberDetail() {
               <CustomForm.InputNumber label="ID" name="id" disabled />
               <CustomForm.Input label="이름" name="name" />
               <CustomForm.GridRow gridColumns="1fr 1fr">
-                <CustomForm.Input label="트랙" name="track" />
-                <CustomForm.Input label="직책" name="position" />
+                <CustomForm.Select label="트랙" name="track" options={SELECT_OPTIONS.track} />
+                <CustomForm.Select label="직책" name="position" options={SELECT_OPTIONS.position} />
               </CustomForm.GridRow>
               <CustomForm.Input label="이메일" name="email" />
               <CustomForm.Input label="학번" name="student_number" />

0a7702919ea98005129f7c62b0024a534da0d9e8
hana <rlagksk8428@gmail.com>
2023-02-10 16:53:44 +0900

    feat: form validate 추가

--- src/pages/UserManage/Member/components/DetailForm.tsx
+++ src/pages/UserManage/Member/components/DetailForm.tsx
@@ -10,13 +10,56 @@ export default function DetailForm({ form }: { form: FormInstance }) {
     <>
       <Divider orientation="left">기본 정보</Divider>
       <CustomForm.InputNumber label="ID" name="id" disabled />
-      <CustomForm.Input label="이름" name="name" />
+      <CustomForm.Input
+        label="이름"
+        name="name"
+        rules={[{
+          required: true,
+          max: 20,
+          message: '최대 20자 이내로 입력해주세요',
+        }]}
+      />
       <CustomForm.GridRow gridColumns="1fr 1fr">
-        <CustomForm.Select label="트랙" name="track" options={SELECT_OPTIONS.track} />
-        <CustomForm.Select label="직책" name="position" options={SELECT_OPTIONS.position} />
+        <CustomForm.Select
+          label="트랙"
+          name="track"
+          options={SELECT_OPTIONS.track}
+          rules={[{
+            required: true,
+            message: '트랙을 선택해주세요',
+          }]}
+        />
+        <CustomForm.Select
+          label="직책"
+          name="position"
+          options={SELECT_OPTIONS.position}
+          rules={[{
+            required: true,
+            message: '직책을 선택해주세요',
+          }]}
+        />
       </CustomForm.GridRow>
-      <CustomForm.Input label="이메일" name="email" />
-      <CustomForm.InputNumber label="학번" name="student_number" />
+      <CustomForm.Input
+        label="이메일"
+        name="email"
+        rules={[{
+          max: 100,
+          message: '최대 100자 이내로 입력해주세요',
+        }]}
+      />
+      <CustomForm.Input
+        label="학번"
+        name="student_number"
+        rules={[
+          {
+            pattern: /^[0-9]+$/,
+            message: '숫자만 입력 가능합니다',
+          },
+          {
+            max: 10,
+            message: '최대 10자 이내로 입력해주세요',
+          }]}
+      />
 
       <Divider orientation="left">사진</Divider>
       <S.UploadWrap>

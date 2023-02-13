bccb9cc90c306b90b39b76ddcde2e72a2d1441e9
hana <rlagksk8428@gmail.com>
2023-02-13 17:15:28 +0900

    feat: mutation 형식 통일, promise 반환 추가

--- src/pages/Services/Room/useRoomMutation.tsx
+++ src/pages/Services/Room/useRoomMutation.tsx
@@ -10,20 +10,42 @@ export default function useRoomMutation(id: number) {
   const navigate = useNavigate();
 
   function deleteRoom() {
-    deleteRoomMutation(id);
-    message.success('삭제되었습니다.');
-    navigate(-1);
+    deleteRoomMutation(id)
+      .unwrap()
+      .then(() => {
+        message.success('삭제되었습니다.');
+        navigate(-1);
+      })
+      .catch(({ data }) => {
+        message.error(data.message);
+      });
   }
 
-  function updateRoom(values: Partial<RoomResponse>) {
-    updateRoomMutation({ id, ...values });
-    message.success('정보 수정이 완료되었습니다.');
-    navigate(-1);
+  function updateRoom(formData: Partial<RoomResponse>) {
+    updateRoomMutation({ id, ...formData })
+      .unwrap()
+      .then(() => {
+        message.success('정보 수정이 완료되었습니다.');
+        navigate(-1);
+      })
+      .catch(({ data }) => {
+        message.error(data.message);
+      });
   }
 
-  function addRoom(values: Partial<RoomResponse>) {
-    addRoomMutation({ ...values });
-    message.success('정보 추가가 완료되었습니다.');
+  function addRoom(formData: Partial<RoomResponse>) {
+    return new Promise((resolve, reject) => {
+      addRoomMutation({ ...formData })
+        .unwrap()
+        .then(() => {
+          message.success('정보 추가가 완료되었습니다.');
+          resolve('성공');
+        })
+        .catch(({ data }) => {
+          message.error(data.message);
+          reject();
+        });
+    });
   }
 
   return { updateRoom, deleteRoom, addRoom } as const;

d347ad013992e36ac1d6297e013e3681f11ccca8
hana <rlagksk8428@gmail.com>
2023-02-17 17:30:33 +0900

    fix: num 네이밍 length로 변경

--- src/utils/hooks/useValidate.ts
+++ src/utils/hooks/useValidate.ts
@@ -3,12 +3,12 @@ export default function useValidate() {
     return { required: true, message: '필수 항목입니다' };
   };
 
-  const Max = (maxNum: number) => {
-    return { max: maxNum, message: `최대 ${maxNum}자 이내로 입력해주세요` };
+  const Max = (maxLength: number) => {
+    return { max: maxLength, message: `최대 ${maxLength}자 이내로 입력해주세요` };
   };
 
-  const Min = (minNum: number) => {
-    return { min: minNum, message: `최소 ${minNum}자 이내로 입력해주세요` };
+  const Min = (minLength: number) => {
+    return { min: minLength, message: `최소 ${minLength}자 이내로 입력해주세요` };
   };
 
   const Pattern = (pattern: RegExp, message: string) => {

6bfaeb2508accfd491923b468d0ca0f5370971cc
hana <rlagksk8428@gmail.com>
2023-02-17 17:33:04 +0900

    fix: return문 삭제

--- src/utils/hooks/useValidate.ts
+++ src/utils/hooks/useValidate.ts
@@ -1,19 +1,11 @@
 export default function useValidate() {
-  const Required = () => {
-    return { required: true, message: '필수 항목입니다' };
-  };
+  const Required = () => ({ required: true, message: '필수 항목입니다' });
 
-  const Max = (maxLength: number) => {
-    return { max: maxLength, message: `최대 ${maxLength}자 이내로 입력해주세요` };
-  };
+  const Max = (maxLength: number) => ({ max: maxLength, message: `최대 ${maxLength}자 이내로 입력해주세요` });
 
-  const Min = (minLength: number) => {
-    return { min: minLength, message: `최소 ${minLength}자 이내로 입력해주세요` };
-  };
+  const Min = (minLength: number) => ({ min: minLength, message: `최소 ${minLength}자 이내로 입력해주세요` });
 
-  const Pattern = (pattern: RegExp, message: string) => {
-    return { pattern, message };
-  };
+  const Pattern = (pattern: RegExp, message: string) => ({ pattern, message });
 
   return {
     Required, Max, Min, Pattern,

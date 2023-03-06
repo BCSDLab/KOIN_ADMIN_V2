725f70236b5c9d0cd5898a7f2c023a09438de8ef
hana <rlagksk8428@gmail.com>
2023-03-06 16:18:16 +0900

    fix: 이름 통일

---
+++ src/constant/store.ts
@@ -0,0 +1,16 @@
+const STORE_OPTION = [
+  {
+    name: '배달 가능',
+    data: 'delivery',
+  },
+  {
+    name: '계좌 이체',
+    data: 'pay_bank',
+  },
+  {
+    name: '카드 결제',
+    data: 'pay_card',
+  },
+] as const;
+
+export default STORE_OPTION;

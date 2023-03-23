2e4ea4b6fcbfda855f4db6ab87b890237bb7a6de
ChoiWonBeen <zad1264@naver.com>
2023-03-24 02:56:35 +0900

    fix: 이미지 필수 표시 추가

--- src/pages/Services/Category/components/DetailForm.tsx
+++ src/pages/Services/Category/components/DetailForm.tsx
@@ -1,5 +1,6 @@
 import CustomForm from 'components/common/CustomForm';
 import { FormInstance } from 'antd/es/form/Form';
+import { Divider } from 'antd';
 
 export default function DetailForm({ form }: { form: FormInstance }) {
   const { required } = CustomForm.useValidate();
@@ -8,6 +9,7 @@ export default function DetailForm({ form }: { form: FormInstance }) {
     <>
       <CustomForm.Input name="id" label="ID" disabled />
       <CustomForm.Input name="name" label="카테고리명" rules={[required()]} />
+      <Divider orientation="left">이미지 (필수)</Divider>
       <CustomForm.SingleUpload name="image_url" domain="shops" form={form} />
     </>
   );

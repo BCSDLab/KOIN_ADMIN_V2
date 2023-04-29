86e68f79a0e8e547a9adab39c694923fbe18e031
ChoiWonBeen <zad1264@naver.com>
2023-04-30 02:16:59 +0900

    style: 세미콜론 빠진 오타 수정

--- src/components/common/CustomTable/index.tsx
+++ src/components/common/CustomTable/index.tsx
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 
 const TableContainer = styled.div`
   padding: 24px 0;
-  width: 100%
+  width: 100%;
   display: flex;
   flex-direction: column;
   .ant-table-thead {

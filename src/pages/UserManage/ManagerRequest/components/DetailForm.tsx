eaf8cdfaf6a97ff848a4ba3a7db13345071ae8c1
dbshaejin <dbshaejin@gmail.com>
2023-06-24 20:27:05 +0900

    fix: owner DetailForm 표 순서 변경

--- src/pages/UserManage/ManagerRequest/components/DetailForm.tsx
+++ src/pages/UserManage/ManagerRequest/components/DetailForm.tsx
@@ -6,14 +6,14 @@ export default function DetailForm() {
       <CustomForm.Input label="id" name="id" disabled />
       <CustomForm.GridRow gridColumns="1fr 0.7fr">
         <CustomForm.Input label="이름" name="name" disabled />
+        <CustomForm.Input label="회원 유형" name="user_type" disabled />
         <CustomForm.Input label="별명" name="nickname" disabled />
+        <CustomForm.Input label="성별" name="gender" disabled />
         <CustomForm.Input label="전화번호" name="phone" disabled />
         <CustomForm.Input label="이메일" name="email" disabled />
-        <CustomForm.Input label="회원 유형" name="user_type" disabled />
-        <CustomForm.Input label="성별" name="gender" disabled />
-        <CustomForm.Input label="사업자 등록 번호" name="company_registration_number" disabled />
-        <CustomForm.Input label="가게 정보" name="shops_id" disabled />
       </CustomForm.GridRow>
+      <CustomForm.Input label="사업자 등록 번호" name="company_registration_number" disabled />
+      <CustomForm.Input label="가게 정보" name="shops_id" disabled />
       <CustomForm.Input label="첨부자료" name="attachments_id" disabled />
     </>
   );

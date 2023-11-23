aef826f3efe19c5909fd97ece99fc0535d98b7c1
김혜준 <alice5855@naver.com>
2023-11-23 21:17:00 +0900

    refactor: 조합 컴포넌트로 추가 변경

--- src/components/common/CustomProForm/CustomProForm.style.tsx
+++ src/components/common/CustomProForm/CustomProForm.style.tsx
@@ -1,10 +1,39 @@
+import { ProForm } from '@ant-design/pro-components';
 import styled from 'styled-components';
+
+export const ProFormWrap = styled(ProForm)`
+  flex-direction: column;
+  `;
 
 export const ProFormListWrap = styled.div` 
   width: fit-content;
   border: 1.5px solid black;
   border-radius: 10px;
   padding: 35px;
+`;
+
+export const FoodItemsWrap = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const FoodSizeItemsWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ResetFoodListButtonWrap = styled.div`
+  display: flex;
+  borderRadius: 5px;
+  margin: 0 auto auto 15px;
+  width: 30px;
+  height: 40px;
+`;
+
+export const ResetFoodSizeButtonWrap = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 export const ProFormTextWrap = styled.div`
--- src/components/common/CustomProForm/CustomProList.tsx
+++ src/components/common/CustomProForm/CustomProList.tsx
@@ -1,7 +1,3 @@
-import {
-  ProForm,
-  ProFormList,
-} from '@ant-design/pro-components';
 import { FormInstance } from 'antd/es/form/Form';
 import { WholeMenuData } from 'model/menus.model';
 import CustomProForm from 'components/common/CustomProForm';
@@ -10,35 +6,18 @@ import * as S from './CustomProForm.style';
 export default function CustomProList({ form }: { form: FormInstance }) {
   const menus: WholeMenuData [] = form.getFieldValue('menu_categories');
   return (
-    <ProForm
-      style={{
-        flexDirection: 'column',
-      }}
+    <CustomProForm.Wrap
       layout="horizontal"
       submitter={{
-        searchConfig: {
-          resetText: '저장',
-          submitText: '초기화',
-        },
-        resetButtonProps: {
-          size: 'middle',
-          style: {
-            backgroundColor: '#1677ff', borderColor: '#1677ff', color: 'white',
-          },
-        },
-        submitButtonProps: {
-          size: 'middle',
-          style: { backgroundColor: '#ff4d4f', borderColor: '#ff4d4f' },
-        },
+        searchConfig: { resetText: '저장', submitText: '초기화' },
+        resetButtonProps: { size: 'middle', style: { backgroundColor: '#1677ff', borderColor: '#1677ff', color: 'white' } },
+        submitButtonProps: { size: 'middle', style: { backgroundColor: '#ff4d4f', borderColor: '#ff4d4f' } },
       }}
     >
       <S.ProFormListWrap>
-        <ProFormList
+        <CustomProForm.List
           name="menus"
-          creatorButtonProps={{
-            creatorButtonText: '메뉴추가',
-            style: { width: 'sm' },
-          }}
+          creatorButtonProps={{ creatorButtonText: '메뉴추가', style: { width: 'sm' } }}
           min={1}
           initialValue={menus[0].menus}
           creatorRecord={{ name: '', singlePrice: '', optionPrices: [{ option: '', price: '' }] }}
@@ -46,51 +25,45 @@ export default function CustomProList({ form }: { form: FormInstance }) {
           copyIconProps={false}
             // eslint-disable-next-line react/no-unstable-nested-components
           itemRender={({ listDom, action }) => (
-            <div style={{ display: 'flex', width: '100%' }}>
-              <div style={{ flex: 1 }}>{listDom}</div>
-              <div style={{
-                display: 'flex',
-                borderRadius: '5px',
-                margin: '0 auto auto 15px',
-                width: '30px',
-                height: '40px',
-              }}
-              >
+            <S.FoodItemsWrap>
+              {listDom}
+              <S.ResetFoodListButtonWrap>
                 {action}
-              </div>
-            </div>
+              </S.ResetFoodListButtonWrap>
+            </S.FoodItemsWrap>
           )}
         >
           <S.ProFormTextWrap>
             <CustomProForm.Text placeholder="메뉴 이름" width="md" name="name" />
             <CustomProForm.Text placeholder="단일 메뉴 가격" width="xs" name="single_price" />
-            <CustomProForm.CardsWrap>
-              <ProFormList
-                name="option_prices"
-                creatorButtonProps={{
-                  creatorButtonText: '사이즈 추가',
-                  style: { width: 'xs' },
-                }}
-                min={1}
-                deleteIconProps={{ tooltipText: '사이즈 삭제' }}
-                copyIconProps={false}
+            <S.CardsWrap>
+              <S.TextsWrap>
+                <CustomProForm.List
+                  name="option_prices"
+                  creatorButtonProps={{ creatorButtonText: '사이즈 추가', style: { width: 'xs' } }}
+                  min={1}
+                  deleteIconProps={{ tooltipText: '사이즈 삭제' }}
+                  copyIconProps={false}
                     // eslint-disable-next-line react/no-unstable-nested-components
-                itemRender={({ listDom, action }) => (
-                  <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                    <div>{listDom}</div>
-                    <div style={{ display: 'flex', alignItems: 'center' }}>{action}</div>
-                  </div>
-                )}
-              >
-                <CustomProForm.TextWrap>
-                  <CustomProForm.Text placeholder="옵션" width="xs" name={['option']} />
-                  <CustomProForm.Text placeholder="가격" width="md" name={['price']} />
-                </CustomProForm.TextWrap>
-              </ProFormList>
-            </CustomProForm.CardsWrap>
+                  itemRender={({ listDom, action }) => (
+                    <S.FoodSizeItemsWrap>
+                      {listDom}
+                      <S.ResetFoodSizeButtonWrap>
+                        {action}
+                      </S.ResetFoodSizeButtonWrap>
+                    </S.FoodSizeItemsWrap>
+                  )}
+                >
+                  <CustomProForm.TextWrap>
+                    <CustomProForm.Text placeholder="옵션" width="xs" name={['option']} />
+                    <CustomProForm.Text placeholder="가격" width="md" name={['price']} />
+                  </CustomProForm.TextWrap>
+                </CustomProForm.List>
+              </S.TextsWrap>
+            </S.CardsWrap>
           </S.ProFormTextWrap>
-        </ProFormList>
+        </CustomProForm.List>
       </S.ProFormListWrap>
-    </ProForm>
+    </CustomProForm.Wrap>
   );
 }

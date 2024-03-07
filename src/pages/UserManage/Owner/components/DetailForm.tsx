e12da27b21a0054c25600c3980b3399eb5c24d07
hana <rlagksk8428@gmail.com>
2024-03-07 23:50:19 +0900

    fix: onwer detail form 항목 수정

--- src/pages/UserManage/Owner/components/DetailForm.tsx
+++ src/pages/UserManage/Owner/components/DetailForm.tsx
@@ -5,16 +5,20 @@ export default function DetailForm({ form }: { form: FormInstance }) {
   return (
     <>
       <CustomForm.Input label="id" name="id" disabled />
-      <CustomForm.GridRow gridColumns="1fr 0.7fr">
-        <CustomForm.Input label="이름" name="name" disabled />
-        <CustomForm.Input label="회원 유형" name="user_type" disabled />
-        <CustomForm.Input label="별명" name="nickname" disabled />
-        <CustomForm.Input label="성별" name="gender" disabled />
-        <CustomForm.Input label="전화번호" name="phone_number" disabled />
-        <CustomForm.Input label="이메일" name="email" disabled />
+      <CustomForm.GridRow gridColumns="1fr 1fr">
+        <CustomForm.Input label="이름" name="name" required />
+        <CustomForm.Input label="회원 유형" name="user_type" />
+        <CustomForm.Input label="별명" name="nickname" />
+        <CustomForm.Input label="성별" name="gender" />
+        <CustomForm.Input label="전화번호" name="phone_number" />
+        <CustomForm.Input label="이메일" name="email" />
+        <CustomForm.Input label="사업자 등록 번호" name="company_registration_number" />
+        <CustomForm.Input label="가게 정보" name="shops_id" />
+        <CustomForm.Input label="계정 생성 날짜" name="created_at" />
+        <CustomForm.Input label="업데이트 날짜" name="updated_at" />
       </CustomForm.GridRow>
-      <CustomForm.Input label="사업자 등록 번호" name="company_registration_number" disabled />
-      <CustomForm.Input label="가게 정보" name="shops_id" disabled />
+      <CustomForm.Input label="마지막 접속 시간" name="last_logged_at" />
+      <CustomForm.Checkbox name="is_authed">사장님 인증 여부</CustomForm.Checkbox>
       <CustomForm.MultipleUpload name="attachments_url" domain="owners" form={form} />
     </>
   );

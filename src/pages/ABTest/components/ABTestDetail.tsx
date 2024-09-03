e8ed3b173f34ee0472064b647106710aabb6ef45
(hoooooony) <(cjh41820@gmail.com)>
2024-09-03 16:04:46 +0900

    feat: 수정 버튼 추가

--- src/pages/ABTest/components/ABTestDetail.tsx
+++ src/pages/ABTest/components/ABTestDetail.tsx
@@ -1,8 +1,10 @@
-import { Divider } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { Button, Divider } from 'antd';
 
 export default function ABTestDetail() {
   return (
     <>
+      <Button icon={<PlusOutlined />}>수정</Button>
       <Divider orientation="left">세부사항</Divider>
       <Divider orientation="left">수동 인원 추가</Divider>
     </>

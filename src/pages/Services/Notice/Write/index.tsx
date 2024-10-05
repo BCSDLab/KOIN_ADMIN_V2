94a6440a86efcbcc5ef56db023e0cc14e880f3fa
MinGu Jeong <mingu830@gmail.com>
2024-10-05 18:59:30 +0900

    feat: 공지사항 글쓰기 UI

--- src/pages/Services/Notice/Write/index.tsx
+++ src/pages/Services/Notice/Write/index.tsx
@@ -1,7 +1,16 @@
+import * as Common from 'styles/List.style';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import '@toast-ui/editor/dist/i18n/ko-kr';
+import NoticeWriteForm from './components/NoticeWriteForm/NoticeWriteForm';
+import * as S from './index.style';
+
 export default function NoticeWritePage() {
   return (
-    <h1>
-      공지사항 글쓰기
-    </h1>
+    <S.Container>
+      <Common.Heading>
+        공지사항 글쓰기
+      </Common.Heading>
+      <NoticeWriteForm />
+    </S.Container>
   );
 }

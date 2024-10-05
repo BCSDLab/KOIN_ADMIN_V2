2d0401885e0a4aa75d73aaf5edfe927f02469ba2
정해성 <gotjd0230@naver.com>
2024-10-05 18:45:07 +0900

    feat: trash 토글 버튼 생성

--- src/pages/Services/Notice/NoticeList.tsx
+++ src/pages/Services/Notice/NoticeList.tsx
@@ -1,8 +1,19 @@
 import { useNavigate } from 'react-router-dom';
 import CustomTable from 'components/common/CustomTable';
 import { useState } from 'react';
-import CustomForm from 'components/common/CustomForm';
+import { Button, Switch } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { styled } from 'styled-components';
 import * as S from './NoticeList.style';
+
+const ActionBar = styled.div`
+  margin-top: 24px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 const temp = {
   notices: [
@@ -22,21 +33,27 @@ const temp = {
 export default function NoticeList() {
   const navigate = useNavigate();
   const [page, setPage] = useState(1);
+  const [showDeleted, setShowDeleted] = useState(false);
 
   return (
     <S.Container>
       <S.Heading>공지사항 목록</S.Heading>
-      <S.ModalWrap>
-        <CustomForm.Modal
-          buttonText="글쓰기"
-          title="글쓰기"
-          width={900}
-          footer={null}
+      <ActionBar>
+        <S.SwitchWrapper>
+          <Switch
+            onClick={setShowDeleted}
+            checked={showDeleted}
+            checkedChildren="trash"
+            unCheckedChildren="trash"
+          />
+        </S.SwitchWrapper>
+        <Button
+          icon={<PlusOutlined />}
           onClick={() => navigate('/notice/write')}
         >
-          .
-        </CustomForm.Modal>
-      </S.ModalWrap>
+          글쓰기
+        </Button>
+      </ActionBar>
       {temp && (
         <CustomTable
           data={temp.notices}

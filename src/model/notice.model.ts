b7ce3e1a917ecca6ac7c968cb377dade47ce352a
정해성 <gotjd0230@naver.com>
2024-10-05 22:41:52 +0900

    feat: api 연결

--- src/model/notice.model.ts
+++ src/model/notice.model.ts
@@ -1,11 +1,12 @@
 import { ListPagination } from './common.model';
 
-export interface OwnerDetailForm extends OwnerResponse {
-  category_ids: number[];
-}
-
 export interface NoticesResponse extends ListPagination {
   notices: Notice[];
+}
+
+export interface TransformedNoticesResponse {
+  notices: TransformedNotice[];
+  total_page: number;
 }
 
 export interface NoticesParam {
@@ -21,38 +22,10 @@ export interface Notice {
   updated_at: string;
 }
 
-export interface OwnerRequestTableHeader {
+export interface TransformedNotice {
   id: number;
-  email: string;
-  name: string;
-  created_at: string;
-  shop_name: string;
-}
-
-export interface OwnerRequestListResponse {
-  owners: OwnerRequestTableHeader[];
-  totalPage: number;
-}
-
-enum UserType {
-  OWNER = 'OWNER',
-  STUDENT = 'STUDENT',
-  USER = 'USER',
-}
-
-export interface OwnerResponse {
-  id: number;
-  nickname: string | null;
-  name: string;
-  phone_number: string | null;
-  user_type: UserType;
-  email: string;
-  gender: string | null;
-  is_authed: boolean;
-  last_logged_at: string;
-  created_at: string;
-  updated_at: string;
-  company_registration_number: string;
-  attachments_id: number[] | null;
-  shops_id: number[] | null;
+  post_number: number;
+  title: string;
+  author: string;
+  post_date: string;
 }
--- src/store/api/notice/index.ts
+++ src/store/api/notice/index.ts
@@ -1,6 +1,6 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import {
-  NoticesParam, NoticesResponse,
+  NoticesParam, NoticesResponse, TransformedNoticesResponse,
 } from 'model/notice.model';
 import baseQueryReauth from 'store/api/baseQueryReauth';
 
@@ -9,11 +9,23 @@ export const noticeApi = createApi({
   tagTypes: ['notices', 'notice'],
   baseQuery: baseQueryReauth,
   endpoints: (builder) => ({
-    getNoticeList: builder.query<NoticesResponse, NoticesParam>({
+    getNoticeList: builder.query<TransformedNoticesResponse, NoticesParam>({
       query: ({ page, is_deleted }) => `admin/notice?page=${page}&limit=1&is_deleted=${is_deleted}`,
       providesTags: (result) => (result
         ? [...result.notices.map((notice) => ({ type: 'notice' as const, id: notice.id })), { type: 'notices', id: 'LIST' }]
         : [{ type: 'notices', id: 'LIST' }]),
+      transformResponse: (res: NoticesResponse): TransformedNoticesResponse => {
+        const notices = res.notices.map((notice) => {
+          return {
+            id: notice.id,
+            post_number: notice.id,
+            title: notice.title,
+            author: notice.author,
+            post_date: notice.created_at,
+          };
+        });
+        return { notices, total_page: res.total_page };
+      },
     }),
 
     // getOwner: builder.query<OwnerResponse, number>({
@@ -30,7 +42,8 @@ export const noticeApi = createApi({
     //       body,
     //     };
     //   },
-    //   invalidatesTags: (result, error, { id }) => [{ type: 'owner', id }, { type: 'owners', id: 'LIST' }],
+    //   invalidatesTags: (result, error, { id }) =>
+    //  [{ type: 'owner', id }, { type: 'owners', id: 'LIST' }],
     // }),
 
     // deleteOwner: builder.mutation<{ success: boolean; id: number }, number>({
@@ -40,7 +53,8 @@ export const noticeApi = createApi({
     //       method: 'DELETE',
     //     };
     //   },
-    //   invalidatesTags: (result, error, id) => [{ type: 'owner', id }, { type: 'owners', id: 'LIST' }],
+    //   invalidatesTags: (result, error, id) =>
+    // [{ type: 'owner', id }, { type: 'owners', id: 'LIST' }],
     // }),
     // owner에서 가져와서 작업하고 있어요. 그쪽 참고하면 될 듯 합니당
   }),

f6cc18ea96215dd63a8d697d71d11705ce0f44aa
MinGu Jeong <mingu830@gmail.com>
2024-10-06 17:17:46 +0900

    feat: 이미지 업로드 구현

--- src/pages/Services/Notice/Write/components/NoticeWriteForm/NoticeWriteForm.tsx
+++ src/pages/Services/Notice/Write/components/NoticeWriteForm/NoticeWriteForm.tsx
@@ -2,14 +2,17 @@
 import CustomForm from 'components/common/CustomForm';
 import { useEffect, useRef } from 'react';
 import { Editor } from '@toast-ui/react-editor';
+import { Form, message } from 'antd';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor/dist/i18n/ko-kr';
 import { UploadOutlined } from '@ant-design/icons';
+import { useUploadfileMutation } from 'store/api/upload';
 import * as S from './NoticeWriteForm.style';
 
 export default function NoticeWriteForm() {
   const { required } = CustomForm.useValidate();
   const editorRef = useRef<Editor | null>(null);
+  const [uploadfile] = useUploadfileMutation();
 
   useEffect(() => {
     const editorInstance = editorRef.current?.getInstance();
@@ -28,25 +31,45 @@ export default function NoticeWriteForm() {
 
   return (
     <S.Container>
-      <S.FormWrapper>
-        <CustomForm.Input label="글번호" name="id" disabled />
-        <CustomForm.Input label="작성자" name="name" rules={[required()]} />
-        <CustomForm.Input label="제목" name="title" rules={[required()]} />
-        <CustomForm.Editor
-          label="본문"
-          name="description"
-          initialEditType="wysiwyg"
-          initialValue=" "
-          height="600px"
-          previewStyle="vertical"
-          useCommandShortcut={false}
-          ref={editorRef}
-          rules={[required()]}
-        />
-      </S.FormWrapper>
-      <S.ButtonWrapper>
-        <CustomForm.Button icon={<UploadOutlined />}>게시</CustomForm.Button>
-      </S.ButtonWrapper>
+      <Form>
+        <S.FormWrapper>
+          <CustomForm.Input label="글번호" name="id" disabled />
+          <CustomForm.Input label="작성자" name="name" disabled />
+          <CustomForm.Input label="게시일" name="date" disabled />
+          <CustomForm.Input label="제목" name="title" rules={[required()]} />
+          <CustomForm.Editor
+            label="본문"
+            name="description"
+            initialEditType="wysiwyg"
+            initialValue=" "
+            height="600px"
+            previewStyle="vertical"
+            useCommandShortcut={false}
+            ref={editorRef}
+            rules={[required()]}
+            hooks={{
+              addImageBlobHook: async (blob, callback) => {
+                try {
+                  const response = await uploadfile({
+                    domain: 'admin',
+                    image: (() => {
+                      const formData = new FormData();
+                      formData.append('multipartFile', blob);
+                      return formData;
+                    })(),
+                  }).unwrap();
+                  callback(response.file_url, '');
+                } catch (error) {
+                  message.error('이미지 업로드에 실패했습니다.');
+                }
+              },
+            }}
+          />
+        </S.FormWrapper>
+        <S.ButtonWrapper>
+          <CustomForm.Button icon={<UploadOutlined />}>게시</CustomForm.Button>
+        </S.ButtonWrapper>
+      </Form>
     </S.Container>
   );
 }

c55646fc34019a877167515868f64ce526436eab
MinGu Jeong <mingu830@gmail.com>
2024-10-06 17:15:42 +0900

    feat: 공지사항 글쓰기 UI

---
+++ src/pages/Services/Notice/Write/index.style.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const Title = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+`;

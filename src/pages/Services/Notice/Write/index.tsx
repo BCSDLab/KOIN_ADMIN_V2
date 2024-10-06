d5dc0d667636cb8e3e91972464267717058c460a
MinGu Jeong <mingu830@gmail.com>
2024-10-06 21:39:10 +0900

    feat: 공지사항 글쓰기 뒤로가기

--- src/pages/Services/Notice/Write/index.tsx
+++ src/pages/Services/Notice/Write/index.tsx
@@ -1,13 +1,18 @@
 import * as Common from 'styles/List.style';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor/dist/i18n/ko-kr';
+import { LeftOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
 import NoticeWriteForm from './components/NoticeWriteForm/NoticeWriteForm';
 import * as S from './index.style';
 
 export default function NoticeWritePage() {
+  const navigate = useNavigate();
+
   return (
     <S.Container>
       <Common.Heading>
+        <LeftOutlined style={{ marginRight: '10px', cursor: 'pointer' }} onClick={() => navigate(-1)} />
         공지사항 글쓰기
       </Common.Heading>
       <NoticeWriteForm />

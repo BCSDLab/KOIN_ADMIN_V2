6f4f08c52600f812da404294cb726f1e94698d7e
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-11-12 23:18:35 +0900

    feat: 비밀번호 변경 모달 생성

---
+++ src/components/common/SideNav/ChangePasswordModal.tsx
@@ -0,0 +1,93 @@
+import sha256 from 'sha256';
+import { message } from 'antd';
+import { useChangePasswordMutation } from 'store/api/auth';
+import useBooleanState from 'utils/hooks/useBoolean';
+import CustomForm from 'components/common/CustomForm';
+
+interface ChangePasswordFormProps {
+  closeModal: () => void,
+}
+
+interface PasswordFormData {
+  currentPassword: string,
+  newPassword: string,
+  checkPassword: string,
+}
+
+export default function ChangePasswordForm({
+  closeModal,
+}: ChangePasswordFormProps) {
+  const {
+    value: isPasswordCorrect, setTrue: correctPassword, setFalse: incorrectPassword,
+  } = useBooleanState(true);
+  const {
+    value: isNewPasswordMatch, setTrue: matchPassword, setFalse: notMatchPassword,
+  } = useBooleanState(true);
+
+  const [changePassword] = useChangePasswordMutation();
+  const [changePasswordForm] = CustomForm.useForm();
+  const { required } = CustomForm.useValidate();
+
+  const handlePasswordMatch = () => {
+    if (changePasswordForm.getFieldValue('newPassword') === changePasswordForm.getFieldValue('checkPassword')) {
+      matchPassword();
+    } else {
+      notMatchPassword();
+    }
+  };
+
+  const onFinish = (formData: PasswordFormData) => {
+    const hashedCurrentPassword = sha256(formData.currentPassword);
+    const hashedNewPassword = sha256(formData.checkPassword);
+    if (formData.newPassword !== formData.checkPassword) {
+      message.error('새 비밀번호를 다시 확인해주세요.');
+      return;
+    }
+    changePassword({ old_password: hashedCurrentPassword, new_password: hashedNewPassword })
+      .unwrap()
+      .then(() => {
+        message.success('비밀번호 변경 완료');
+        correctPassword();
+        closeModal();
+        changePasswordForm.resetFields();
+      })
+      .catch(({ data }) => {
+        message.error(data.message);
+        incorrectPassword();
+      });
+  };
+
+  return (
+    <CustomForm form={changePasswordForm} onFinish={onFinish}>
+      <CustomForm.Input
+        label="현재 비밀번호"
+        name="currentPassword"
+        type="password"
+        rules={[required()]}
+      />
+      {
+        !isPasswordCorrect
+        && <div style={{ color: 'red' }}>현재 비밀번호가 일치하지 않습니다.</div>
+      }
+      <CustomForm.Input
+        label="새 비밀번호"
+        name="newPassword"
+        type="password"
+        onChange={handlePasswordMatch}
+        rules={[required()]}
+      />
+      <CustomForm.Input
+        label="새 비밀번호 확인"
+        name="checkPassword"
+        type="password"
+        onChange={handlePasswordMatch}
+        rules={[required()]}
+      />
+      {
+        !isNewPasswordMatch
+        && <div style={{ color: 'red' }}>새 비밀번호와 일치하지 않습니다.</div>
+      }
+      <CustomForm.Button htmlType="submit">변경하기</CustomForm.Button>
+    </CustomForm>
+  );
+}

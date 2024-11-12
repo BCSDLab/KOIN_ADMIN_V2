4939b2b1e2ccb06f48bb087dc53d6cb38cf656b7
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-11-12 16:51:39 +0900

    fix: 로그아웃 시 refreshToken 삭제

--- src/utils/hooks/useLogout.ts
+++ src/utils/hooks/useLogout.ts
@@ -2,8 +2,9 @@ import { setCredentials } from 'store/slice/auth';
 
 export default function useLogout() {
   const logout = () => {
-    setCredentials({ token: '' });
     sessionStorage.removeItem('token');
+    localStorage.removeItem('refresh_token');
+    setCredentials({ token: '' });
     window.location.href = '/login';
   };
   return logout;

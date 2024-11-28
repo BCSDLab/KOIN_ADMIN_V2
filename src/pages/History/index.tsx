837082c6ea906a07dfa896378964fb5327704a8a
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-11-28 23:57:09 +0900

    feat: 로그 히스토리 페이지 생성

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,5 +1,41 @@
+import { Flex, Pagination } from 'antd';
+import { useState } from 'react';
+import { useGetHistorysQuery } from 'store/api/history';
+import * as S from './History.style';
+
 export default function History() {
+  const [page, setPage] = useState<number>(1);
+
+  const { data: historys } = useGetHistorysQuery({ page });
+  const pagination = {
+    current: page,
+    onChange: setPage,
+    total: historys ? historys.total_page : 0,
+  };
+  const historyList = historys ? historys.historys : [];
+
   return (
-    <div>History</div>
+    <Flex vertical>
+      <S.Heading>로그 히스토리</S.Heading>
+      <S.HistoryContainer>
+        {
+          historyList.map((history) => (
+            <S.HistoryItem>
+              {`${history.created_at}. ${history.domain_name} ${history.request_method} (${history.name})`}
+            </S.HistoryItem>
+          ))
+        }
+      </S.HistoryContainer>
+      {pagination && pagination.total > 0 && (
+        <Pagination
+          current={pagination.current}
+          total={pagination.total * 10}
+          onChange={pagination.onChange}
+          showSizeChanger={false}
+          showQuickJumper
+          align="center"
+        />
+      )}
+    </Flex>
   );
 }

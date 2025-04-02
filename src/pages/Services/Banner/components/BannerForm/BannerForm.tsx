fb3e7fdeaa870fa2d9b1907e1c633a44cbcfd091
ff1451 <ff1451@gmail.com>
2025-04-03 00:55:20 +0900

    fix: 이미지 외의 파일 업로드 제한

--- src/pages/Services/Banner/components/BannerForm/BannerForm.tsx
+++ src/pages/Services/Banner/components/BannerForm/BannerForm.tsx
@@ -75,7 +75,7 @@ export default function BannerForm({ form, isEdit, categoryOptions }: BannerForm
         이미지
       </Divider>
 
-      <CustomForm.SingleUpload form={form} name="image_url" domain="banner" />
+      <CustomForm.SingleImageUpload form={form} name="image_url" domain="banner" />
 
       <Divider orientation="left" style={{ marginTop: '40px' }} />
 

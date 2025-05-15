eadfbf807761e55849c23a0bbe005a71a8f75f6b
이준영 <ff1451@gmail.com>
2025-05-15 17:59:02 +0900

    feat: 동아리 목록 UI 구현

---
+++ src/pages/Services/Club/ClubList.style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export * from 'styles/List.style';
+
+export const ClubContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;

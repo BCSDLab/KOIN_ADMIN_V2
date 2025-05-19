78d38876b2467e633abd87ffce665a21447949f4
이준영 <ff1451@gmail.com>
2025-05-20 01:28:45 +0900

    feat: 승인 대기 관리자 리스트 및 상세 정보 슬라이드 구현

---
+++ src/pages/UserManage/ClubManagerRequest/Components/RequestInfo/RequestInfo.style.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Title = styled.div`
+  font-size: 40px;
+  `;
+
+export const SectionTitle = styled.div`
+  font-size: 28px;
+  margin-bottom: 0px;
+  margin-top: 20px;
+  `;
+
+export const Content = styled.div`
+  font-size: 16px;
+  margin-bottom: 10px;
+  `;

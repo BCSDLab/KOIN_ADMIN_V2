7966b47dd70a004f507a5efb4042943a011bc8fb
이준영 <ff1451@gmail.com>
2025-05-23 21:16:59 +0900

    refactor: 폴더 구조 변경 및 lint 에러 수정

--- src/pages/Services/Club/Components/CustomColumn/CustomColumns.tsx
+++ src/pages/Services/Club/Components/CustomColumn/CustomColumns.tsx
@@ -1,6 +1,6 @@
 import type { ColumnType } from 'antd/es/table';
-import ClubManagerList from 'pages/Services/Club/Components/ClubManagerList/ClubManagerList';
-import ClubActiveSwitch from 'pages/Services/Club/Components/ClubActiveSwitch/ClubActiveSwitch';
+import ClubManagerList from 'pages/Services/Club/Components/CustomColumn/ClubManagerList/ClubManagerList';
+import ClubActiveSwitch from 'pages/Services/Club/Components/CustomColumn/ClubActiveSwitch/ClubActiveSwitch';
 
 interface Club {
   id: number;

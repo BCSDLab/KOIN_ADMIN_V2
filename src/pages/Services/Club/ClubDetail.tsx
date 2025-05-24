d6c7c5b1650891a1a6b4d18a0027a5aa990339ed
이준영 <ff1451@gmail.com>
2025-05-24 19:41:28 +0900

    feat: 동아리 생성 및 수정 API 연결

--- src/pages/Services/Club/ClubDetail.tsx
+++ src/pages/Services/Club/ClubDetail.tsx
@@ -1,16 +1,28 @@
 import { Flex } from 'antd';
+import { useMemo } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useGetClubCategoryListQuery, useGetClubQuery } from 'store/api/club';
+import {
+  ClubUpdateFormValues, ClubUpdateRequest,
+} from 'model/club.model';
 import CustomBreadcrumb from 'components/common/CustomBreadCrumb';
 import CustomForm from 'components/common/CustomForm';
 import DetailHeading from 'components/common/DetailHeading';
-// import { useParams } from 'react-router-dom';
-import * as S from 'styles/Detail.style';
 import useBooleanState from 'utils/hooks/useBoolean';
 import ConfirmModal from 'components/ConfirmModal/ConfirmModal';
+import * as S from 'styles/Detail.style';
 import ClubForm from './Components/ClubForm/ClubForm';
+import useClubMutation from './useClubMutation';
 
 export default function ClubDetail() {
-  // const { id } = useParams();
+  const navigate = useNavigate();
+
+  const { id } = useParams();
   const [form] = CustomForm.useForm();
+
+  const { data: ClubData, isLoading, isError } = useGetClubQuery(Number(id));
+  const { data: ClubCategory } = useGetClubCategoryListQuery();
+  const { updateClub } = useClubMutation();
 
   const {
     setTrue: openUpdateModal,
@@ -31,7 +43,93 @@ export default function ClubDetail() {
 
   const handleCancel = () => {
     closeCancelModal();
+    navigate(-1);
   };
+
+  const clubCategoryOptions: Record<string, string> = useMemo(() => (
+    ClubCategory
+      ? ClubCategory.club_categories.reduce((categoryMap: Record<string, string>, clubCategory) => {
+        categoryMap[String(clubCategory.id)] = clubCategory.name;
+        return categoryMap;
+      }, {} as Record<string, string>)
+      : {}
+  ), [ClubCategory]);
+
+  const initialValues = useMemo(() => {
+    if (!ClubData || !clubCategoryOptions) return undefined;
+
+    const firstManager = ClubData.club_managers?.[0] ?? {};
+    const managerFields = {
+      user_id: firstManager.user_id,
+      manager_name: firstManager.name,
+      manager_phone: firstManager.phone_number,
+    };
+
+    const snsFields = ClubData.sns_contacts?.reduce((acc, contact) => {
+      switch (contact.sns_type) {
+        case '전화 번호':
+          acc.phone_number = contact.contact;
+          break;
+        case '인스타그램':
+          acc.instagram = contact.contact;
+          break;
+        case '구글 폼':
+          acc.google_form = contact.contact;
+          break;
+        case '오픈 채팅':
+          acc.open_chat = contact.contact;
+          break;
+        default:
+          break;
+      }
+      return acc;
+    }, {} as Record<string, string>);
+
+    const categoryId = Object.entries(clubCategoryOptions).find(
+      ([, name]) => name === ClubData.club_category_name,
+    )?.[0];
+
+    return {
+      ...ClubData,
+      ...managerFields,
+      ...snsFields,
+      club_category_id: categoryId ? Number(categoryId) : undefined,
+    };
+  }, [ClubData, clubCategoryOptions]);
+
+  const handleFinish = (values : ClubUpdateFormValues) => {
+    const payload: ClubUpdateRequest = {
+      ...values,
+      club_managers: [{
+        user_id: values.user_id,
+      }],
+    };
+    updateClub(payload);
+  };
+
+  const handleValuesChange = (changedValues: any) => {
+    if ('club_category_name' in changedValues) {
+      const selectedId = changedValues.club_category_name;
+      form.setFieldsValue({
+        club_category_id: Number(selectedId),
+      });
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <>
+      </>
+    );
+  }
+
+  if (!ClubData || isError) {
+    return (
+      <S.Container>
+        <DetailHeading>해당 ID의 동아리가 존재하지 않습니다.</DetailHeading>
+      </S.Container>
+    );
+  }
 
   return (
     <S.Container>
@@ -40,12 +138,17 @@ export default function ClubDetail() {
         items={[
           { label: 'ClubList', path: '/club' },
           { label: 'ClubDetail' },
-          // { label: ClubData.title },
+          { label: ClubData!.name },
         ]}
       />
       <S.FormWrap>
-        <CustomForm form={form}>
-          <ClubForm form={form} />
+        <CustomForm
+          form={form}
+          onFinish={handleFinish}
+          initialValues={initialValues}
+          onValuesChange={handleValuesChange}
+        >
+          <ClubForm form={form} categoryOptions={clubCategoryOptions} />
         </CustomForm>
         <Flex justify="end" gap="10px">
           <CustomForm.Modal

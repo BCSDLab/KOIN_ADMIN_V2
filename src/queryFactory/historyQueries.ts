a8ba57148a4a5e553547b08933894ac203860054
강성훈 <kshevl123@gmail.com>
2025-09-29 23:19:27 +0900

    feat: history 마이그레이션

---
+++ src/queryFactory/historyQueries.ts
@@ -0,0 +1,24 @@
+import { queryOptions } from '@tanstack/react-query';
+import getHistories from 'api/history';
+import { HistoriesRequest } from 'model/history.model';
+
+const historyQueries = {
+  allKeys: () => ['histories'] as const,
+  historyKeys: (
+    {
+      page,
+      domainId = null,
+      limit = 30,
+    }: HistoriesRequest,
+  ) => [
+    ...historyQueries.allKeys(),
+    page, domainId, limit,
+  ] as const,
+  history: (options: HistoriesRequest) => queryOptions({
+    queryKey: historyQueries.historyKeys(options),
+    queryFn: () => getHistories(options),
+    refetchOnMount: true,
+  }),
+};
+
+export default historyQueries;

b8bc38ca0c2228d7dbf4c330be387c53dc09fbaa
hyejun <hyejunkkim228@gmail.com>
2025-09-29 18:17:25 +0900

    refactor: 오타 수정

--- src/pages/Services/Category/components/DetailForm.tsx
+++ src/pages/Services/Category/components/DetailForm.tsx
@@ -20,7 +20,7 @@ export default function DetailForm({ form }: { form: FormInstance }) {
       <CustomForm.Input name="id" label="ID" disabled />
       <CustomForm.Input name="name" label="카테고리명" rules={[required()]} />
       <CustomForm.Select name="parent_category_id" label="상위 카테고리" rules={[required()]} disabled={false} options={options} />
-      <Divider orientation="left">이미지sdfsd (필수)</Divider>
+      <Divider orientation="left">이미지 (필수)</Divider>
       <CustomForm.SingleUpload name="image_url" domain="shops" form={form} />
     </>
   );

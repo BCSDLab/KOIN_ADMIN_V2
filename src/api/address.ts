ce6ebe763bf33389760be47c00b456976a051618
dooohun <ehgns0305@naver.com>
2025-09-30 02:12:11 +0900

    fix: getAddressSearch 함수에 export 삭제

--- src/api/address.ts
+++ src/api/address.ts
@@ -1,7 +1,7 @@
 import accessClient from 'api';
 import { AddressSearchRequest, AddressSearchResponse } from 'model/address.model';
 
-export const getAddressSearch = async ({ keyword, currentPage = '1', countPerPage = '10' }: AddressSearchRequest) => {
+const getAddressSearch = async ({ keyword, currentPage = '1', countPerPage = '10' }: AddressSearchRequest) => {
   const { data } = await accessClient.get<AddressSearchResponse>('/address/search', {
     params: { keyword, currentPage, countPerPage },
   });

bc65f7d58f603a3d103cb542c895bcd615ed2cbd
강성훈 <kshevl123@gmail.com>
2025-09-29 23:11:03 +0900

    refactor: rtk apiList 정리

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,42 +1,28 @@
 import { Middleware, configureStore } from '@reduxjs/toolkit';
 import authReducer from 'store/slice/auth';
 import { authApi } from './api/auth';
-import { userApi } from './api/user';
 import { roomApi } from './api/room';
-import { memberApi } from './api/member';
 import { uploadApi } from './api/upload';
 import { storeApi } from './api/store';
 import { storeMenuApi } from './api/storeMenu';
 import { menuCategoriesApi } from './api/storeMenu/category';
-import { noticeApi } from './api/notice';
-import { ownerRequestApi } from './api/ownerRequest';
-import { ownerApi } from './api/owner';
-import { reviewApi } from './api/review';
 import { abTestApi } from './api/abtest';
 import { benefitApi } from './api/benefit';
 import { updateListApi } from './api/updateList';
-import { historyApi } from './api/history';
 import { bannerApi } from './api/banner';
 import { bannerCategoryApi } from './api/bannerCategory';
 import { addressApi } from './api/address';
 
 const apiList = [
   authApi,
-  userApi,
   roomApi,
-  memberApi,
   uploadApi,
   storeApi,
   storeMenuApi,
   menuCategoriesApi,
-  noticeApi,
-  ownerRequestApi,
-  ownerApi,
-  reviewApi,
   abTestApi,
   benefitApi,
   updateListApi,
-  historyApi,
   bannerApi,
   bannerCategoryApi,
   addressApi,

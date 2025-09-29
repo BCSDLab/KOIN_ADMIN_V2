56cbe8d587fab6e33932b48999a336a71e37812e
dooohun <ehgns0305@naver.com>
2025-09-30 02:12:21 +0900

    fix: allKey 에서 allKeys로 변경

--- src/queryFactory/addressQueries.ts
+++ src/queryFactory/addressQueries.ts
@@ -1,15 +1,16 @@
 import { queryOptions } from '@tanstack/react-query';
+import getAddressSearch from 'api/address';
 import { AddressSearchRequest } from 'model/address.model';
 
 const addressQueries = {
-  allKey: () => ['address'],
+  allKeys: () => ['address'],
 
-  addressSearchKey: (keyword: string, currentPage: string, countPerPage: string) => [...addressQueries.allKey(), 'search', keyword, currentPage, countPerPage],
-  addressSearch: ({ keyword, currentPage = '1', countPerPage = '10' }: AddressSearchRequest) => (queryOptions({
+  addressSearchKey: (keyword: string, currentPage: string, countPerPage: string) => [...addressQueries.allKeys(), 'search', keyword, currentPage, countPerPage],
+  addressSearch: ({ keyword, currentPage = '1', countPerPage = '10' }: AddressSearchRequest) => queryOptions({
     queryKey: addressQueries.addressSearchKey(keyword, currentPage, countPerPage),
-    queryFn: () => import('api/address').then(({ getAddressSearch }) => getAddressSearch({ keyword, currentPage, countPerPage })),
+    queryFn: () => getAddressSearch({ keyword, currentPage, countPerPage }),
     enabled: !!keyword,
-  })),
+  }),
 };
 
 export default addressQueries;

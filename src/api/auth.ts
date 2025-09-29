2f9badb25b78ec7912f7360230492bee476a4b48
dooohun <ehgns0305@naver.com>
2025-09-29 23:10:11 +0900

    feat: auth api, auth query factory 추가

---
+++ src/api/auth.ts
@@ -0,0 +1,29 @@
+import accessClient from 'api';
+import axios from 'axios';
+import {
+  AdminInfo, ChangePasswordRequest, LoginRequest, LoginResponse,
+} from 'model/auth.model';
+
+export const apiClient = axios.create({
+  baseURL: `${import.meta.env.VITE_API_PATH}`,
+  timeout: 100000,
+});
+
+export const postLogin = async ({ email, password }: LoginRequest) => {
+  const { data } = await apiClient.post<LoginResponse>('admin/user/login', {
+    email: `${email}@koreatech.ac.kr`,
+    password,
+  });
+
+  return data;
+};
+
+export const getAdminInfo = async () => {
+  const { data } = await accessClient.get<AdminInfo>('admin');
+
+  return data;
+};
+
+export const changePassword = async ({ old_password, new_password }: ChangePasswordRequest) => {
+  await accessClient.put('admin/password', { old_password, new_password });
+};
---
+++ src/queryFactory/authQueries.ts
@@ -0,0 +1,12 @@
+import { queryOptions } from '@tanstack/react-query';
+import { getAdminInfo } from 'api/auth';
+
+const authQueries = {
+  allKeys: () => ['auth'],
+
+  adminInfoKey: () => [...authQueries.allKeys(), 'admin', 'info'],
+  adminInfo: () => queryOptions({
+    queryKey: authQueries.adminInfoKey(),
+    queryFn: getAdminInfo,
+  }),
+};

f2cab70ed758240152283ef3b6d3369cf66f3b2d
강성훈 <kshevl123@gmail.com>
2025-11-28 17:58:10 +0900

    feat: bus model 추가

---
+++ src/model/bus.model.ts
@@ -0,0 +1,31 @@
+export type Semester = 'REGULAR' | 'SEASONAL' | 'VACATION';
+
+export type ProgressType = 'initial' | 'selectedSemester' | 'selectedFile' | 'completeUpload';
+
+export interface BusRouteInfo {
+  node_info: Node[];
+  region: string;
+  route_info: Route[];
+  route_name: string;
+  route_type: string;
+  sub_name: string | null;
+}
+
+interface Node {
+  name: string;
+  detail: null | string;
+}
+
+interface Route {
+  arrival_time: (string | null)[];
+  detail: null | string;
+  name: string;
+}
+
+export interface CommutingBusRouteInfoResponse {
+  commuting_bus_timetables: BusRouteInfo[];
+}
+
+export interface ShuttleBusRouteInfoResponse {
+  shuttle_bus_timetables: BusRouteInfo[];
+}

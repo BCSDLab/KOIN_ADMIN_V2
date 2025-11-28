25a81ac4e9067f3161064dfd811b1f170f7ae9f0
강성훈 <kshevl123@gmail.com>
2025-11-28 17:59:26 +0900

    feat: 등하교 버스 mutation 추가

---
+++ src/pages/Update/CommutingBusUpdate/useCommutingBusUpdateMutation.ts
@@ -0,0 +1,24 @@
+import { useMutation } from '@tanstack/react-query';
+import type { CommutingBusRouteInfoResponse, Semester } from 'model/bus.model';
+import { postCommutingBusTimetable, putCommutingBusTimetable } from 'api/commutingBusUpdate';
+
+const useCommutingBusUpdateMutation = () => {
+  const uploadCommutingBusTimetable = useMutation({
+    mutationFn: (formData: FormData) => postCommutingBusTimetable(formData),
+  });
+
+  const migrateCommutingBusTimetable = useMutation({
+    mutationFn: ({ uploadedData, semesterType }: {
+      uploadedData: CommutingBusRouteInfoResponse;
+      semesterType: Semester
+    }) => putCommutingBusTimetable(uploadedData, semesterType),
+  });
+
+  return {
+    uploadCommutingBusTimetable,
+    uploadedData: uploadCommutingBusTimetable.data,
+    migrateCommutingBusTimetable,
+  };
+};
+
+export default useCommutingBusUpdateMutation;

7fb6eb4387909086d3bff2904e2855a522ada982
강성훈 <kshevl123@gmail.com>
2025-11-30 00:04:24 +0900

    style: css 파일 공백 정리

--- src/pages/Update/ShuttleBusUpdate/components/ShuttleBusPreview.style.tsx
+++ src/pages/Update/ShuttleBusUpdate/components/ShuttleBusPreview.style.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 100%;
   overflow: scroll;
+  font-family: Pretendard, sans-serif;
 `;
 
 export const Main = styled.div`
@@ -35,51 +36,49 @@ export const GridContainer = styled.div<{ $columns: number, $rows: number }>`
   grid-template-columns: repeat(${(props) => props.$columns}, 1fr);
   grid-template-rows: repeat(${(props) => props.$rows}, 1fr);
   grid-auto-flow: column;
-  margin-top:30px;
+  margin-top: 30px;
 `;
 
 export const NodeWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding-left: 24px;
-    padding-right: 12px;
-    min-height: 54px;
-    gap: 8px; //여기 다름
-    color: #000;
-    border-right: 2.5px solid #f5f5f5;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 24px;
+  padding-right: 12px;
+  min-height: 54px;
+  gap: 8px; //여기 다름
+  color: #000;
+  border-right: 2.5px solid #f5f5f5;
 `;
 
 export const Standard = styled.div`
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    min-width: 110px;
-    padding: 8px;
-    justify-content: center;
-    text-align: center;
-    gap: 8px;
-    color: #4b4b4b;
-    background-color: #f5f5f5;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  min-width: 110px;
+  padding: 8px;
+  justify-content: center;
+  text-align: center;
+  gap: 8px;
+  color: #4b4b4b;
+  background-color: #f5f5f5;
 `;
 
 export const Detail = styled.div`
-    color: #727272;
-    font-family: Pretendard, sans-serif;
-    font-size: 12px;
-    font-weight: 400;
+  color: #727272;
+  font-size: 12px;
+  font-weight: 400;
 `;
 
 export const Time = styled.div`
-    display: flex;
-    padding: 8px 0;
-    justify-content: center;
-    align-items: center;
-    font-family: Pretendard, sans-serif;
-    font-size: 18px;
-    font-weight: 600;
-    `;
+  display: flex;
+  padding: 8px 0;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  font-weight: 600;
+`;
 
 export const TimeColumn = styled.div`
   display: contents;
-  `;
+`;

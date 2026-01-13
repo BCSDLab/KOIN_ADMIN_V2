2ee05e028978183f11e1676e6fedcd4bdef16131
James(곽승주) <tmdwn10165@jinhakapply.com>
2026-01-13 11:03:43 +0900

    feat: admin 관련 api 훅 생성

---
+++ src/pages/UserManage/Admin/useAdminMutation.ts
@@ -0,0 +1,39 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { message } from 'antd';
+import { changeAdminInfo, createAdmin } from 'api/admin';
+import type { Admin, SignUpAdminRequest } from 'model/admin.model';
+import adminQueries from 'queryFactory/adminQueries';
+import { useNavigate } from 'react-router-dom';
+
+export default function useAdminMutation(id?: number) {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const updateAdminMutation = useMutation({
+    mutationFn: (formData: Partial<Admin>) => changeAdminInfo(id!, formData),
+    onSuccess: () => {
+      message.success('정보 수정이 완료되었습니다.');
+      queryClient.invalidateQueries({ queryKey: adminQueries.allKeys() });
+      navigate(-1);
+    },
+    onError: (error) => {
+      message.error(error.message || '에러가 발생했습니다.');
+    },
+  });
+
+  const createAdminMutation = useMutation({
+    mutationFn: (formData: SignUpAdminRequest) => createAdmin(formData),
+    onSuccess: () => {
+      message.success('관리자 계정이 생성되었습니다.');
+      queryClient.invalidateQueries({ queryKey: adminQueries.allKeys() });
+    },
+    onError: (error) => {
+      message.error(error.message || '에러가 발생했습니다.');
+    },
+  });
+
+  return {
+    updateAdminMutation,
+    createAdminMutation,
+  };
+}
---
+++ src/queryFactory/adminQueries.ts
@@ -0,0 +1,31 @@
+import { queryOptions } from '@tanstack/react-query';
+import { getAdminInfo, getAdminList } from 'api/admin';
+import type { AdminListRequest } from 'model/admin.model';
+
+interface AdminListParams {
+  page?: number;
+  trackName?: AdminListRequest['trackName'];
+  teamName?: AdminListRequest['teamName'];
+}
+
+const adminQueries = {
+  allKeys: () => ['admin'],
+
+  adminListKeys: (params: AdminListParams) => [...adminQueries.allKeys(), 'list', params],
+  adminList: (params: AdminListParams) => queryOptions({
+    queryKey: adminQueries.adminListKeys(params),
+    queryFn: () => getAdminList({
+      page: params.page,
+      trackName: params.trackName,
+      teamName: params.teamName,
+    }),
+  }),
+
+  adminInfoKeys: (id: number) => [...adminQueries.allKeys(), id],
+  adminInfo: (id: number) => queryOptions({
+    queryKey: adminQueries.adminInfoKeys(id),
+    queryFn: () => getAdminInfo(id),
+  }),
+};
+
+export default adminQueries;
